b2a1e720d25f4b674f392166c63ec637eab7aa7a
RutvikS-crest <rutvik.shiyani@crestdatasys.com>
2021-08-16 18:11:20 +0530

    Added support for dhcp_option_policy for ACI Terraformer

---
+++ providers/aci/dhcp_option_policy.go
@@ -0,0 +1,52 @@
+package aci
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/GoogleCloudPlatform/terraformer/terraformutils"
+)
+
+const dhcpOptionPolicyClassName = "dhcpOptionPol"
+
+type DhcpOptionPolicyGenerator struct {
+	ACIService
+}
+
+func (a *DhcpOptionPolicyGenerator) InitResources() error {
+	client, err := a.createClient()
+	if err != nil {
+		return err
+	}
+
+	baseURL := "/api/node/class"
+	dnURL := fmt.Sprintf("%s/%s.json", baseURL, dhcpOptionPolicyClassName)
+
+	DhcpOptionPoliciesCont, err := client.GetViaURL(dnURL)
+	if err != nil {
+		return err
+	}
+
+	DhcpOptionPolicyCount, err := strconv.Atoi(stripQuotes(DhcpOptionPoliciesCont.S("totalCount").String()))
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < DhcpOptionPolicyCount; i++ {
+		DhcpOptionPolicyDN := DhcpOptionPoliciesCont.S("imdata").Index(i).S(dhcpOptionPolicyClassName, "attributes", "dn").String()
+		resource := terraformutils.NewSimpleResource(
+			stripQuotes(DhcpOptionPolicyDN),
+			stripQuotes(DhcpOptionPolicyDN),
+			"aci_dhcp_option_policy",
+			"aci",
+			[]string{
+				"name_alias",
+				"dhcp_option",
+				"dhcp_option_ids",
+			},
+		)
+		resource.SlowQueryRequired = true
+		a.Resources = append(a.Resources, resource)
+	}
+	return nil
+}
