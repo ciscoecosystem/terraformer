5f12862d33afbd3b5da1050af7f02bc7b7087d78
Rutvik Shiyani <88027613+RutvikS-crest@users.noreply.github.com>
2021-11-15 11:46:42 +0530

    Aci service redirect policy (#60)
    * service redirect policy and destination of redirected traffic Added
    
    * done indentetion
    
    Co-authored-by: harshthakkar_crest <thakkar.harsh@crestdatasys.com>

---
+++ providers/aci/service_redirect_policy.go
@@ -0,0 +1,58 @@
+package aci
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/GoogleCloudPlatform/terraformer/terraformutils"
+)
+
+const serviceRedirectPolicyClassName = "vnsSvcRedirectPol"
+
+type ServiceRedirectPolicyGenerator struct {
+	ACIService
+}
+
+func (a *ServiceRedirectPolicyGenerator) InitResources() error {
+	client, err := a.createClient()
+	if err != nil {
+		return err
+	}
+
+	baseURL := "/api/node/class"
+	dnURL := fmt.Sprintf("%s/%s.json", baseURL, serviceRedirectPolicyClassName)
+
+	ServiceRedirectPolicyCont, err := client.GetViaURL(dnURL)
+	if err != nil {
+		return err
+	}
+
+	ServiceRedirectPolicyCount, err := strconv.Atoi(stripQuotes(ServiceRedirectPolicyCont.S("totalCount").String()))
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < ServiceRedirectPolicyCount; i++ {
+		ServiceRedirectPolicyAttr := ServiceRedirectPolicyCont.S("imdata").Index(i).S(serviceRedirectPolicyClassName, "attributes")
+		ServiceRedirectPolicyDN := G(ServiceRedirectPolicyAttr, "dn")
+		name := G(ServiceRedirectPolicyAttr, "name")
+		if filterChildrenDn(ServiceRedirectPolicyDN, client.parentResource) != "" {
+			resource := terraformutils.NewResource(
+				ServiceRedirectPolicyDN,
+				ServiceRedirectPolicyDN,
+				"aci_service_redirect_policy",
+				"aci",
+				map[string]string{
+					"tenant_dn": GetParentDn(ServiceRedirectPolicyDN, fmt.Sprintf("/svcRedirectPol-%s", name)),
+				},
+				[]string{
+					"description",
+				},
+				map[string]interface{}{},
+			)
+			resource.SlowQueryRequired = true
+			a.Resources = append(a.Resources, resource)
+		}
+	}
+	return nil
+}
