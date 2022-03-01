04c43f18a6a5c8edfe4c15c7a014917031b9dba2
harshthakkar_crest <thakkar.harsh@crestdatasys.com>
2022-03-01 12:31:24 +0530

    added schema_site for mso

--- providers/mso/mso_provider.go
+++ providers/mso/mso_provider.go
@@ -76,6 +76,7 @@ func (p *MSOProvider) InitService(serviceName string, verbose bool) error {
 
 func (p *MSOProvider) GetSupportedService() map[string]terraformutils.ServiceGenerator {
 	return map[string]terraformutils.ServiceGenerator{
-		"schema": &SchemaGenerator{},
+		"schema":      &SchemaGenerator{},
+		"schema_site": &SchemaSiteGenerator{},
 	}
 }
