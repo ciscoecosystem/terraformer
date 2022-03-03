4f819f4dbcff8ad5bcc8cfcecd707ddf7d76618d
harshthakkar_crest <thakkar.harsh@crestdatasys.com>
2022-03-03 11:46:34 +0530

    initial commit for mso_schema_template_l3out

---
+++ providers/mso/mso_schema_template_l3out.go
@@ -0,0 +1,67 @@
+package mso
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/GoogleCloudPlatform/terraformer/terraformutils"
+	"github.com/ciscoecosystem/mso-go-client/client"
+	"github.com/ciscoecosystem/mso-go-client/models"
+)
+
+type SchemaTemplateL3OutGenerator struct {
+	MSOService
+}
+
+func (a *SchemaTemplateL3OutGenerator) InitResources() error {
+	mso := a.getClient().(*client.Client)
+	con, err := mso.GetViaURL("api/v1/schemas/")
+	if err != nil {
+		return err
+	}
+	schemaLen := len(con.S("schemas").Data().([]interface{}))
+	for i := 0; i < schemaLen; i++ {
+		schemaCont := con.S("schemas").Index(i)
+		schemaId := models.G(schemaCont, "id")
+		templateLen := 0
+		if schemaCont.Exists("templates") {
+			templateLen = len(schemaCont.S("templates").Data().([]interface{}))
+		}
+		for j := 0; j < templateLen; j++ {
+			templateCont := schemaCont.S("templates").Index(j)
+			templateName := models.G(templateCont, "name")
+			l3outLen := 0
+			if templateCont.Exists("intersiteL3outs") {
+				l3outLen = len(templateCont.S("intersiteL3outs").Data().([]interface{}))
+			}
+			for k := 0; k < l3outLen; k++ {
+				l3outCont := templateCont.S("intersiteL3outs").Index(k)
+				l3outName := models.G(l3outCont, "name")
+				displayName := models.G(l3outCont, "displayName")
+				vrfRef := models.G(l3outCont, "vrfRef")
+				vrfRefSplitted := strings.Split(vrfRef, "/")
+				vrfName := vrfRefSplitted[len(vrfRefSplitted)-1]
+				name := strconv.Itoa(i) + "_" + strconv.Itoa(j) + "_" + strconv.Itoa(k)
+				resource := terraformutils.NewResource(
+					l3outName,
+					name,
+					"mso_schema_template_l3out",
+					"mso",
+					map[string]string{
+						"schema_id":     schemaId,
+						"template_name": templateName,
+						"l3out_name":    l3outName,
+						"display_name":  displayName,
+						"vrf_name":      vrfName,
+					},
+					[]string{},
+					map[string]interface{}{},
+				)
+				resource.SlowQueryRequired = true
+				a.Resources = append(a.Resources, resource)
+
+			}
+		}
+	}
+	return nil
+}
